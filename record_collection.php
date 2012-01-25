2da1d0f18d46f548e6f4adbc9e9152a9945ece50
Andhika Nugraha <andhika.nugraha@gmail.com>
2012-01-25 10:30:45 +0700

    Modified HeliumRecordCollection
    * Tidied up code and added some documentation
    * Added method chaining

--- record_collection.php
+++ record_collection.php
@@ -53,6 +53,11 @@ class HeliumRecordCollection implements Iterator {
 	private $index = 0;
 	private $prepared_index = 0;
 
+	/**
+	 * Constructor
+	 *
+	 * @param string $class_name Name of the class of the record.
+	 */
 	public function __construct($class_name) {
 		$this->class_name = $class_name;
 		$this->model_name = Inflector::underscore($class_name);
@@ -64,6 +69,12 @@ class HeliumRecordCollection implements Iterator {
 		$this->many_to_many_associations = $prototype->_associations['many-to-many'];
 	}
 
+	/**
+	 * Query manipulation methods
+	 *
+	 * Each of these methods support method chaining
+	 */
+
 	public function include_associations() {
 		$this->set_order_by('id');
 
@@ -73,45 +84,137 @@ class HeliumRecordCollection implements Iterator {
 		foreach ($this->one_to_one_associations as $association_id => $options) {
 			$this->include_association($association_id);
 		}
-		
+
 		foreach ($this->many_to_many_associations as $association_id => $options) {
 			extract($options);
 			$join_condition = "`$join_table`.`$local_key`=`$local_table`.id";
 			$this->join_statements[] = sprintf($base_join_statement, $join_table, $join_condition);
 		}
+
+		return $this;
 	}
 
 	public function include_association($association_id) {
-		if ($this->included_associations[$association_id])
-			return;
+		if (!$this->included_associations[$association_id]) {
+			$base_join_statement = ' LEFT JOIN `%s` ON %s';
+			$local_table = $this->table_name;
 
-		$base_join_statement = ' LEFT JOIN `%s` ON %s';
-		$local_table = $this->table_name;
+			if ($options = $this->one_to_one_associations[$association_id]) {
+				// prepare join statement
+				extract($options);
+				$foreign_table = $this->get_model_table($class_name);
+				if ($foreign_table) {
+					if ($_type == 'belongs_to')
+						$join_condition = "`$foreign_table`.`id`=`$local_table`.`$foreign_key`";
+					else
+						$join_condition = "`$foreign_table`.`$foreign_key`=`$local_table`.`id`";
+					$this->join_statements[] = sprintf($base_join_statement, $foreign_table, $join_condition);
+				}
 
-		if ($options = $this->one_to_one_associations[$association_id]) {
-			// prepare join statement
-			extract($options);
-			$foreign_table = $this->get_model_table($class_name);
-			if ($foreign_table) {
-				if ($_type == 'belongs_to')
-					$join_condition = "`$foreign_table`.`id`=`$local_table`.`$foreign_key`";
-				else
-					$join_condition = "`$foreign_table`.`$foreign_key`=`$local_table`.`id`";
-				$this->join_statements[] = sprintf($base_join_statement, $foreign_table, $join_condition);
+				// prepare sibling column retrieval
+				$db = Helium::db();
+				$foreign_columns = $db->get_col("SHOW COLUMNS FROM `$foreign_table`");
+				foreach ($foreign_columns as $col) {
+					$this->add_additional_column(".{$association_id}.{$col}", "`$foreign_table`.`$col`");
+				}
+
+				$options['_foreign_columns'] = $foreign_columns;
+				$this->included_associations[$association_id] = $options;
 			}
-
-			// prepare sibling column retrieval
-			$db = Helium::db();
-			$foreign_columns = $db->get_col("SHOW COLUMNS FROM `$foreign_table`");
-			foreach ($foreign_columns as $col) {
-				$this->add_additional_column(".{$association_id}.{$col}", "`$foreign_table`.`$col`");
-			}
-
-			$options['_foreign_columns'] = $foreign_columns;
-			$this->included_associations[$association_id] = $options;
 		}
+
+		return $this;
 	}
 
+	public function set_conditions_array(Array $conditions_array) {
+		$this->fetched = false;
+		$this->conditions_string = $this->generate_conditions_string($conditions_array);
+
+		return $this;
+	}
+
+	public function set_conditions_string($conditions_string) {
+		$this->fetched = false;
+		$this->conditions_string = trim($conditions_string);
+
+		return $this;
+	}
+
+	public function set_batch_length($batch_length) {
+		$this->fetched = false;
+		$this->batch_length = $batch_length;
+
+		return $this;
+	}
+
+	public function set_batch_number($batch_number) {
+		$this->fetched = false;
+		$this->batch_number = $batch_number;
+		$this->batch_start = ($batch_number - 1) * $this->batch_length;
+
+		return $this;
+	}
+
+	public function set_order($order) {
+		$this->fetched = false;
+		$this->order = strtoupper($order);
+
+		return $this;
+	}
+
+	public function set_order_by() {
+		$this->fetched = false;
+
+		$fields = func_get_args();
+		$escaped_fields = array();
+
+		foreach ($fields as $field) {
+			$escaped_fields[] = $this->escape_field($field);
+		}
+
+		$this->order_by = implode(', ', $escaped_fields);
+
+		return $this;
+	}
+
+	public function add_additional_column($name, $declaration) {
+		$this->additional_columns[$name] = $declaration;
+
+		return $this;
+	}
+
+	public function widen($conditions) {
+		$this->fetched = false;
+		if (is_array($conditions))
+			$conditions = $this->generate_conditions_string($conditions);
+		$this->conditions_string .= ' OR ' . $conditions;
+
+		return $this;
+	}
+
+	public function narrow($conditions) {
+		$this->fetched = false;
+		if (is_array($conditions))
+			$conditions = $this->generate_conditions_string($conditions);
+		if ($this->conditions_string == '1')
+			$this->conditions_string = $conditions;
+		else
+			$this->conditions_string .= ' AND ' . $conditions;
+
+		return $this;
+	}
+
+	public function add_ID($id) {
+		$id = (string) Helium::numval($id);
+		$this->widen(array('id' => $id));
+
+		return $this;
+	}
+
+	/**
+	 * SQL generation methods
+	 */
+	
 	private function get_model_table($class_name = '') {
 		if (class_exists($class_name)) {
 			$prototype = new $class_name(false);
@@ -125,7 +228,7 @@ class HeliumRecordCollection implements Iterator {
 		$base_query = 'SELECT `%s`.*%s FROM `%1$s`%s WHERE %s ORDER BY %s %s';
 
 		$join_clause = implode('', $this->join_statements);
-		
+
 		$additionals = '';
 		foreach ($this->additional_columns as $col => $dec) {
 			$additionals .= ", ($dec) AS `$col`";
@@ -138,6 +241,163 @@ class HeliumRecordCollection implements Iterator {
 
 		return $query;
 	}
+
+	private function generate_conditions_string(Array $array) {
+		if (!$array) // empty array
+			return '';
+
+		$db = Helium::db();
+
+		$query = array();
+        foreach ($array as $field => $value) {
+			if (strpos($field, '(') > 0) { // functions as fields
+				// TODO: add escaping code for the field
+			}
+			elseif ($dec = $this->additional_columns[$field])
+				$field = "({$dec})";
+			else
+				$field = $this->escape_field($field);
+			$value = $db->escape($value);
+            $query[] = "{$field}='{$value}'";
+		}
+		$conditions_string = '(' . implode(" AND ", $query) . ')';
+
+		return $conditions_string;
+	}
+
+	private function escape_field($field) {
+		$field_particles = explode('.', $field);
+
+		if (count($field_partices) < 2)
+			array_unshift($field_particles, $this->table_name);
+
+		array_walk($field_particles, function(&$particle) {
+			$particle = "`$particle`";
+		});
+
+		$field = implode('.', $field_particles);
+
+		return $field;
+	}
+
+	/**
+	 * Counting methods
+	 */
+
+	public function count() {
+		$this->fetch();
+
+		return $this->count;
+	}
+
+	public function count_all() {
+		if ($this->count_all && $this->fetched)
+			return $this->count_all;
+
+		$db = Helium::db();
+
+		// make the query
+		$base_query = 'SELECT COUNT(*) FROM `%s`%s WHERE %s';
+
+		$join_clause = implode('', $this->join_statements);
+
+		$query = sprintf($base_query, $this->table_name, $join_clause, $this->conditions_string);
+
+		$count = $db->get_var($query);
+
+		return $count;
+	}
+
+	public function get_number_of_batches() {
+		if (!$this->batch_length)
+			return 1;
+		else
+			return ceil($this->count_all() / $this->batch_length);
+	}
+
+	/**
+	 * Record preparation methods
+	 */
+
+	private function prepare_record($row) {
+		$class_name = $this->class_name;
+		$record = new $class_name(false);
+
+		foreach ($this->col_types as $name => $type) {
+			$raw_value = $row[$name];
+			$prepared_value = $this->prepare_value($raw_value, $type);
+			$record->$name = $prepared_value;
+		}
+
+		foreach ($this->additional_columns as $col => $dec) {
+			$record->$col = $row[$col];
+		}
+
+		foreach ($this->included_associations as $association_id => $options) {
+			$class_name = $options['class_name'];
+			$associate = new $class_name;
+			foreach ($options['_foreign_columns'] as $col) {
+				$raw_name = '.' . $association_id . '.' . $col;
+				$associate->$col = $row[$raw_name];
+			}
+			$associate->_exists = true;
+			$associate->_associate = $record;
+			$associate->rebuild();
+			$associate->_unserialize_auto();
+
+			$record->$association_id = $associate;
+		}
+
+		$record->_column_types = $this->col_types;
+		$record->_associate = $this->_associate;
+		$record->_exists = true;
+		$record->rebuild();
+		$record->_unserialize_auto();
+
+		return $record;
+	}
+
+	public static function prepare_value($value, $type) {
+		switch ($type) {
+			case 'bool':
+				$value = (bool) $value;
+				break;
+			case 'int':
+				$value = Helium::numval($value);
+				break;
+			case 'float':
+				$value = floatval($value);
+				break;
+			case 'datetime':
+				$value = new HeliumDateTime($value);
+				break;
+			// default: $value is a string, let it be
+		}
+
+		return $value;
+	}
+
+	public function delete_all() {
+		if ($this->count_all && $this->fetched)
+			return $this->count_all;
+
+		$db = Helium::db();
+
+		// make the query
+		$base_query = 'DELETE FROM `%s`%s WHERE %s';
+
+		$join_clause = implode('', $this->join_statements);
+
+		$query = sprintf($base_query, $this->table_name, $join_clause, $this->conditions_string);
+
+		$count = $db->get_var($query);
+
+		return $count;
+	}
+
+	/**
+	 * Database retrieval methods
+	 */
 
 	public function fetch() {
 		if ($this->fetched)
@@ -214,44 +474,6 @@ class HeliumRecordCollection implements Iterator {
 		}
 	}
 
-	private function generate_conditions_string(Array $array) {
-		if (!$array) // empty array
-			return '';
-
-		$db = Helium::db();
-
-		$query = array();
-        foreach ($array as $field => $value) {
-			if (strpos($field, '(') > 0) { // functions as fields
-				// TODO: add escaping code for the field
-			}
-			elseif ($dec = $this->additional_columns[$field])
-				$field = "({$dec})";
-			else
-				$field = $this->escape_field($field);
-			$value = $db->escape($value);
-            $query[] = "{$field}='{$value}'";
-		}
-		$conditions_string = '(' . implode(" AND ", $query) . ')';
-
-		return $conditions_string;
-	}
-
-	private function escape_field($field) {
-		$field_particles = explode('.', $field);
-
-		if (count($field_partices) < 2)
-			array_unshift($field_particles, $this->table_name);
-
-		array_walk($field_particles, function(&$particle) {
-			$particle = "`$particle`";
-		});
-
-		$field = implode('.', $field_particles);
-
-		return $field;
-	}
-
 	public function first() {
 		if (!$this->fetched) {
 			$bl = $this->batch_length;
@@ -268,181 +490,9 @@ class HeliumRecordCollection implements Iterator {
 		return $this->current();
 	}
 
-	public function set_conditions_array(Array $conditions_array) {
-		$this->fetched = false;
-		$this->conditions_string = $this->generate_conditions_string($conditions_array);
-	}
-
-	public function set_conditions_string($conditions_string) {
-		$this->fetched = false;
-		$this->conditions_string = trim($conditions_string);
-	}
-
-	public function set_batch_length($batch_length) {
-		$this->fetched = false;
-		$this->batch_length = $batch_length;
-	}
-
-	public function set_batch_number($batch_number) {
-		$this->fetched = false;
-		$this->batch_number = $batch_number;
-		$this->batch_start = ($batch_number - 1) * $this->batch_length;
-	}
-
-	public function set_order($order) {
-		$this->fetched = false;
-		$this->order = strtoupper($order);
-	}
-
-	public function set_order_by() {
-		$this->fetched = false;
-		
-		$fields = func_get_args();
-		$escaped_fields = array();
-
-		foreach ($fields as $field) {
-			$escaped_fields[] = $this->escape_field($field);
-		}
-
-		$this->order_by = implode(', ', $escaped_fields);
-	}
-
-	public function count() {
-		$this->fetch();
-
-		return $this->count;
-	}
-
-	public function count_all() {
-		if ($this->count_all && $this->fetched)
-			return $this->count_all;
-
-		$db = Helium::db();
-
-		// make the query
-		$base_query = 'SELECT COUNT(*) FROM `%s`%s WHERE %s';
-
-		$join_clause = implode('', $this->join_statements);
-
-		$query = sprintf($base_query, $this->table_name, $join_clause, $this->conditions_string);
-
-		$count = $db->get_var($query);
-
-		return $count;
-	}
-
-	public function get_number_of_batches() {
-		if (!$this->batch_length)
-			return 1;
-		else
-			return ceil($this->count_all() / $this->batch_length);
-	}
-
-	public function add_additional_column($name, $declaration) {
-		$this->additional_columns[$name] = $declaration;
-	}
-
-	public function widen($conditions) {
-		$this->fetched = false;
-		if (is_array($conditions))
-			$conditions = $this->generate_conditions_string($conditions);
-		$this->conditions_string .= ' OR ' . $conditions;
-	}
-
-	public function narrow($conditions) {
-		$this->fetched = false;
-		if (is_array($conditions))
-			$conditions = $this->generate_conditions_string($conditions);
-		if ($this->conditions_string == '1')
-			$this->conditions_string = $conditions;
-		else
-			$this->conditions_string .= ' AND ' . $conditions;
-	}
-
-	public function add_ID($id) {
-		$id = (string) Helium::numval($id);
-		$this->widen(array('id' => $id));
-	}
-
-	private function prepare_record($row) {
-		$class_name = $this->class_name;
-		$record = new $class_name(false);
-
-		foreach ($this->col_types as $name => $type) {
-			$raw_value = $row[$name];
-			$prepared_value = $this->prepare_value($raw_value, $type);
-			$record->$name = $prepared_value;
-		}
-
-		foreach ($this->additional_columns as $col => $dec) {
-			$record->$col = $row[$col];
-		}
-		
-		foreach ($this->included_associations as $association_id => $options) {
-			$class_name = $options['class_name'];
-			$associate = new $class_name;
-			foreach ($options['_foreign_columns'] as $col) {
-				$raw_name = '.' . $association_id . '.' . $col;
-				$associate->$col = $row[$raw_name];
-			}
-			$associate->_exists = true;
-			$associate->_associate = $record;
-			$associate->rebuild();
-			$associate->_unserialize_auto();
-
-			$record->$association_id = $associate;
-		}
-
-		$record->_column_types = $this->col_types;
-		$record->_associate = $this->_associate;
-		$record->_exists = true;
-		$record->rebuild();
-		$record->_unserialize_auto();
-
-		return $record;
-	}
-
-	public static function prepare_value($value, $type) {
-		switch ($type) {
-			case 'bool':
-				$value = (bool) $value;
-				break;
-			case 'int':
-				$value = Helium::numval($value);
-				break;
-			case 'float':
-				$value = floatval($value);
-				break;
-			case 'datetime':
-				$value = new HeliumDateTime($value);
-				break;
-			// default: $value is a string, let it be
-		}
-
-		return $value;
-	}
-
-	public function delete_all() {
-		if ($this->count_all && $this->fetched)
-			return $this->count_all;
-
-		$db = Helium::db();
-
-		// make the query
-		$base_query = 'DELETE FROM `%s`%s WHERE %s';
-
-		$join_clause = implode('', $this->join_statements);
-
-		$query = sprintf($base_query, $this->table_name, $join_clause, $this->conditions_string);
-
-		$count = $db->get_var($query);
-
-		return $count;
-	}
-
-	// iterator methods
-	// we're only using numerical indices
-	// so there's no need to use array_keys() like on php.net.
+	/**
+	 * Iterator methods
+	 */
 
 	public function rewind() {
 		$this->fetch();
